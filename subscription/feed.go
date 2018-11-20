9612caee12f1bb0543712f2b56e2b8207a72768d
Toru Maesaka <toru@torumk.com>
2018-11-19 21:32:38 -0800

    subscription: shrink the detection space

--- subscription/feed.go
+++ subscription/feed.go
@@ -5,10 +5,10 @@ import (
 )
 
 const atomHint = "http://www.w3.org/2005/Atom"
-const detectionLimit = 2048
+const detectionLimit = 1024
 
 // Determines if the document is an Atom feed by searching for the
-// format namespace. Search is given up after 2048 bytes.
+// format namespace. Search is given up after 1024 bytes.
 func isAtomFeed(doc []byte) bool {
 	upper := len(doc)
 	if upper > detectionLimit {
