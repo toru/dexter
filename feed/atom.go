62ac1017e5796feed1b04375f871255db7070390
Toru Maesaka <toru@torumk.com>
2018-11-21 15:25:46 -0800

    feed: draft out the atom feed struct

--- feed/atom.go
+++ feed/atom.go
@@ -2,10 +2,30 @@ package feed
 
 import (
 	"bytes"
+	"time"
 )
 
 const atomHint = "http://www.w3.org/2005/Atom"
 const detectionLimit = 1024
+
+// AtomFeed represents the top-level container element of the Atom
+// feed document as described in RFC 4287 Section 4.1.1
+type AtomFeed struct {
+	// Author
+	// Category
+	// Contributor
+	// Generator
+	Icon string `xml:"icon"`
+	ID   string `xml:"id"`
+	// Links
+	Logo     string    `xml:"logo"`
+	Rights   string    `xml:"rights"`
+	Subtitle string    `xml:"subtitle"`
+	Title    string    `xml:"title"`
+	Updated  time.Time `xml:"updated"`
+	// Extentions
+	// Entries
+}
 
 // Heuristically determines if the document is an Atom feed by searching
 // for the format namespace. Search is given up after 1024 bytes.
