11381c74f99a9b1eb8370126ab6185041d55d247
Toru Maesaka <toru@torumk.com>
2018-12-01 19:53:58 -0800

    feed: implement the atom:generator element

--- feed/atom.go
+++ feed/atom.go
@@ -9,22 +9,31 @@ import (
 const atomHint = "http://www.w3.org/2005/Atom"
 const detectionLimit = 1024
 
-// AtomAuthor represents an element described in RFC 4287 Section 4.2.1
+// AtomAuthor represents the "atom:author" element defined in
+// RFC 4287 Section 4.2.1
 type AtomAuthor struct {
 	Name  string `xml:"name"`
 	URI   string `xml:"uri"`
 	Email string `xml:"email"`
 }
 
+// AtomGenerator represents the "atom:generator" element defined in
+// RFC 4287 Section 4.2.4
+type AtomGenerator struct {
+	URI     string `xml:"uri,attr"`
+	Version string `xml:"version,attr"`
+	Value   string `xml:",chardata"`
+}
+
 // AtomFeed represents the top-level container element of the Atom
-// feed document as described in RFC 4287 Section 4.1.1
+// feed document as defined in RFC 4287 Section 4.1.1
 type AtomFeed struct {
 	Author AtomAuthor `xml:"author"`
 	// Category
 	// Contributor
-	// Generator
-	Icon string `xml:"icon"`
-	ID   string `xml:"id"`
+	Generator AtomGenerator `xml:"generator"`
+	Icon      string        `xml:"icon"`
+	ID        string        `xml:"id"`
 	// Links
 	Logo     string    `xml:"logo"`
 	Rights   string    `xml:"rights"`
