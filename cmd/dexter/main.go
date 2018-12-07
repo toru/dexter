2086f4bcc2eebd30eb149bfa055d45df8a40622c
Toru Maesaka <toru@torumk.com>
2018-12-06 20:51:43 -0800

    cmd/dexter: require a -cfg command-line option

--- cmd/dexter/main.go
+++ cmd/dexter/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
+	var cfgPath string
+	flag.StringVar(&cfgPath, "cfg", "", "Path to the config file (required)")
+	flag.Parse()
+
+	if len(cfgPath) == 0 {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
 	fmt.Println("starting dexter")
 	for range time.Tick(time.Second) {
 		fmt.Printf("tick: %d\n", time.Now().Unix())
