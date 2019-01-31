a2422435b7997c7ed9c11cbb16accad8048c62f7
Toru Maesaka <toru@torumk.com>
2019-01-30 19:22:47 -0800

    store: add a map of subscriptions

--- store/memory.go
+++ store/memory.go
@@ -1,8 +1,15 @@
 package store
 
+import (
+	"github.com/toru/dexter/subscription"
+)
+
+// MemoryStore is a simple memory-backed storage engine.
 type MemoryStore struct {
+	subscriptions map[string]subscription.Subscription
 }
 
+// Name returns the name of the storage engine.
 func (s MemoryStore) Name() string {
 	return "Memory Store"
 }
