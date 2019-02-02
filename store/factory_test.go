4c12435f2fd939253bd66c39497d8ecfb704505e
Toru Maesaka <toru@torumk.com>
2019-02-01 20:21:44 -0800

    store: update the factory test

--- store/factory_test.go
+++ store/factory_test.go
@@ -8,12 +8,17 @@ func TestGetStore(t *testing.T) {
 	t.Run("with in-memory store", func(t *testing.T) {
 		expectedName := "Memory Store"
 
-		s, ok := GetStore("memory")
-		if !ok {
-			t.Error("Got: true, Want: false")
+		s, err := GetStore("memory")
+		if err != nil {
+			t.Error(err)
 		}
 		if name := s.Name(); name != expectedName {
 			t.Errorf("Got: %s, Want: %s", name, expectedName)
+		}
+	})
+	t.Run("with bogus store", func(t *testing.T) {
+		if _, err := GetStore("bogus"); err == nil {
+			t.Errorf("Got: nil, Want: error")
 		}
 	})
 }
