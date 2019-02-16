59c1eea5c028f31783dddf3d746ddbd520495abd
Toru Maesaka <toru@torumk.com>
2019-02-16 11:44:03 -0800

    web: first cut of the subscriptions handler

--- web/app.go
+++ web/app.go
@@ -1,6 +1,8 @@
 package web
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,6 +17,33 @@ type ServerConfig struct {
 	Port   uint   // TCP port to listen for Web API requests
 }
 
+type subscriptionPresenter struct {
+	ID  string `json:"id"`  // Hex representation of the ID
+	URL string `json:"url"` // FeedURL as a string
+}
+
+// GET /subscriptions
+// Renders a list of subscriptions
+func subscriptionsHandlerFunc(db store.Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		subs := make([]subscriptionPresenter, 0, db.NumSubscriptions())
+		for _, sub := range db.Subscriptions() {
+			subs = append(subs, subscriptionPresenter{
+				hex.EncodeToString(sub.ID[:]),
+				sub.FeedURL.String(),
+			})
+		}
+
+		buf, err := json.Marshal(subs)
+		if err != nil {
+			log.Print(err)
+			http.Error(w, "response payload", http.StatusInternalServerError)
+			return
+		}
+		w.Write(buf)
+	}
+}
+
 // ServeWebAPI starts the Web API application.
 func ServeWebAPI(cfg ServerConfig, db store.Store) error {
 	log.Println("starting the web api server")
@@ -22,6 +51,8 @@ func ServeWebAPI(cfg ServerConfig, db store.Store) error {
 		log.Printf("port missing, using: %d", defaultPort)
 		cfg.Port = defaultPort
 	}
+
+	http.Handle("/subscriptions", subscriptionsHandlerFunc(db))
 
 	// TODO(toru): TLS
 	addr := fmt.Sprintf("%s:%d", cfg.Listen, cfg.Port)
