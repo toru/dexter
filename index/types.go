e13d0f21911be5163645214352c5cb8f18cbf227
Toru Maesaka <toru@torumk.com>
2019-03-23 12:44:14 -0700

    index: add an api to create a DexID from a string

--- index/types.go
+++ index/types.go
@@ -8,3 +8,8 @@ import (
 // DexID is a 224-bit (or 28-byte) long primary key that every
 // object inside Dexter has been alloted with.
 type DexID = [sha256.Size224]byte
+
+// NewDexIDFromString returns a new DexID based on the given string.
+func NewDexIDFromString(src string) DexID {
+	return sha256.Sum224([]byte(src))
+}
