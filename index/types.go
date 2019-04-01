5ac253af8dbf7516176bd6eba88c9d17f17b5656
Toru Maesaka <toru@torumk.com>
2019-03-31 18:12:45 -0700

    index: add a utility func to hexdump a DexID

--- index/types.go
+++ index/types.go
@@ -3,6 +3,7 @@ package index
 
 import (
 	"crypto/sha256"
+	"encoding/hex"
 )
 
 // DexID is a 224-bit (or 28-byte) long primary key that every
@@ -12,4 +13,10 @@ type DexID = [sha256.Size224]byte
 // NewDexIDFromString returns a new DexID based on the given string.
 func NewDexIDFromString(src string) DexID {
 	return sha256.Sum224([]byte(src))
+}
+
+// DexIDToHexDigest returns the hexadecimal representation of the given
+// DexID as a string. Sadly, DexID can't be used as a method receiver.
+func DexIDToHexDigest(id DexID) string {
+	return hex.EncodeToString(id[:])
 }
