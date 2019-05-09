c5df5129dde3d62588007d7f5b05f0fba13e7363
Toru Maesaka <toru@torumk.com>
2019-05-08 20:04:10 -0700

    feed: rename the test file to match the rearrangement

--- feed/detector_test.go
+++ feed/detector_test.go
@@ -4,16 +4,17 @@ import (
 	"testing"
 )
 
+var rssDoc = []byte(`<?xml version="1.0" encoding="UTF-8"?><rss version="2.0"></rss>`)
+var atomDoc = []byte(`<?xml version="1.0" encoding="UTF-8"?><feed xmlns="http://www.w3.org/2005/Atom"></feed>`)
+
 func TestIsAtomFeed(t *testing.T) {
 	t.Run("with an atom document", func(t *testing.T) {
-		atomDoc := []byte(`<feed xmlns="http://www.w3.org/2005/Atom"></feed>`)
 		if !IsAtomFeed(atomDoc) {
 			t.Errorf("Got: false, Want: true")
 		}
 	})
 
 	t.Run("with an rss document", func(t *testing.T) {
-		rssDoc := []byte(`<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"></rss>`)
 		if IsAtomFeed(rssDoc) {
 			t.Errorf("Got: true, Want: false")
 		}
