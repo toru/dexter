80d2282253626a1d218e6be06afeb2d309eec53d
Toru Maesaka <toru@torumk.com>
2019-09-16 15:27:17 -0700

    index/sha224_test: NewIDFromHexDigest is deprecated

--- index/sha224_test.go
+++ index/sha224_test.go
@@ -1,7 +1,6 @@
 package index
 
 import (
-	"bytes"
 	"testing"
 )
 
@@ -15,18 +14,5 @@ func TestSetValueFromHexString(t *testing.T) {
 	src = "ab66ac985b784cd6966b63122df2f73fc756aef8599530f9011a2c14"
 	if err := id.SetValueFromHexString(src); err != nil {
 		t.Error(err)
-	}
-}
-
-func TestNewIDFromHexDigest(t *testing.T) {
-	orig := NewDexIDFromString("dexter")
-	digest := DexIDToHexDigest(orig)
-
-	repro, err := NewDexIDFromHexDigest(digest)
-	if err != nil {
-		t.Error(err)
-	}
-	if !bytes.Equal(repro[:], orig[:]) {
-		t.Errorf("Got: %x, Want: %x", repro, orig)
 	}
 }
