e50a1bc21e745686c1f6ba806fbe10b0df584894
Toru Maesaka <toru@torumk.com>
2019-10-06 05:36:21 -0700

    index/issuer: store the hash algorithm type

--- index/issuer.go
+++ index/issuer.go
@@ -5,12 +5,15 @@ import (
 )
 
 type Issuer struct {
+	hashAlgo uint8
 }
 
+// NewIssuer returns a new ID Issuer
 func NewIssuer(algo string) (Issuer, error) {
 	isr := Issuer{}
 	if !IsSupported(algo) {
 		return isr, fmt.Errorf("unsupported hash algorithm")
 	}
+	isr.hashAlgo = hashAlgoDict[algo]
 	return isr, nil
 }
