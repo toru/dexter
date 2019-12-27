161787ff0007cd50a63953b8350d60b47ee233ca
Toru Maesaka <toru@torumk.com>
2019-12-27 15:03:14 -0800

    index/sha1: scaffold the addresser

---
+++ index/sha1.go
@@ -0,0 +1,18 @@
+/*
+ * WIP: SHA-1 based object addresser.
+ */
+
+package index
+
+import (
+	"crypto/sha1"
+)
+
+const (
+	SHA1DexIDLen    = sha1.Size
+	SHA1DexIDHexLen = sha1.Size * 2
+)
+
+type SHA1DexID struct {
+	value [SHA1DexIDLen]byte
+}
